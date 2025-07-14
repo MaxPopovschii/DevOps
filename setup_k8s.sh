02a5e963c988fdc95f56f97e79a7871da5b4897b
MaxPopovschii <122737792+MaxPopovschii@users.noreply.github.com>
2025-07-14 12:16:56 +0200

    Update setup_k8s.sh

--- setup_k8s.sh
+++ setup_k8s.sh
@@ -3,10 +3,15 @@
 # Exit on any error
 set -e
 
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run with sudo -E $0" 1>&2
+   exit 1
+fi
+
 # Default values
 HOST=$(hostname)
 DOCKER_VERSION="latest" 
-KUBELET_VERSION="1.28.10-1.1" 
 CNI_PLUGINS_VERSION="v1.2.0" 
 FLANNEL_VERSION="latest"  
 METALLB_VERSION="v0.14.3" 
@@ -14,7 +19,7 @@ NGINX_HELM_VERSION="9.7.7"
 POD_NETWORK_CIDR="10.244.0.0/16" 
 GATEWAY_IP="192.168.1.1" 
 STATIC_IPS=("192.168.1.174/32 192.168.1.175/32 192.168.1.176/32")
-KUB_VERSION="v1.28"
+KUB_VERSION="1.28.10"
 NET_IP="192.168.1.172/24"
 INTERFACE="enp0s8"
 
@@ -23,7 +28,6 @@ function usage {
     echo "Usage: $0 [OPTIONS]"
     echo "Options:"
     echo "  -k, --kub-version       Kubernetes version (default: $KUB_VERSION)"
-    echo "  -kl, --kubelet-version  Kubernetes instruments version (default: $KUBELET_VERSION)"
     echo "  -c, --cni-plugins       CNI plugins version (default: $CNI_PLUGINS_VERSION)"
     echo "  -f, --flannel-version   Flannel version (default: $FLANNEL_VERSION)"
     echo "  -m, --metallb-version   MetalLB version (default: $METALLB_VERSION)"
@@ -42,10 +46,6 @@ while [[ "$1" != "" ]]; do
         -k | --kub-version )
             shift
             KUB_VERSION=$1
-            ;;
-        -kl | --kubelet-version )
-            shift
-            KUBELET_VERSION=$1
             ;;
         -c | --cni-plugins )
             shift
@@ -66,6 +66,10 @@ while [[ "$1" != "" ]]; do
         -n | --network-ip )
             shift
             NET_IP=$1
+            ;;
+        -i | --interface )
+            shift
+            INTERFACE=$1
             ;;
         -p | --pod-network-cidr )
             shift
@@ -90,92 +94,93 @@ done
 # System cleanup function
 function system_cleanup {
     echo "Cleaning up and updating the system..."
-    sudo apt clean all
-    sudo apt update
-    sudo apt upgrade -y
-    sudo apt dist-upgrade -y
-    sudo apt autoremove -y
-    sudo apt autoclean -y
+    apt clean all
+    apt update
+    apt upgrade -y
+    apt dist-upgrade -y
+    apt autoremove -y
+    apt autoclean -y
 }
 
 # Disable swap function
 function disable_swap {
     echo "Disabling swap..."
-    sudo swapoff -a
-    sudo rm -rf /swap.img
-    sudo sed -i '/swap/s/^/#/' /etc/fstab
+    swapoff -a
+    rm -rf /swap.img
+    sed -i '/swap/s/^/#/' /etc/fstab
 }
 
 # Enable IP forwarding function
 function enable_ip_forwarding {
     echo "Enabling IP forwarding..."
-    sudo sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
-    sudo sysctl -w net.ipv4.ip_forward=1
-    sudo sysctl -p
-    sudo modprobe overlay
-    sudo modprobe br_netfilter
+    sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
+    sysctl -w net.ipv4.ip_forward=1
+    sysctl -p
+    modprobe overlay
+    modprobe br_netfilter
 
-    cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+    cat <<EOF | tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
 EOF
 
-    cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+    cat <<EOF | tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
 EOF
 
-    sudo sysctl --system
+    sysctl --system
 }
 
 # Remove old Docker installations function
 function remove_docker {
     echo "Removing old Docker installations..."
-    sudo apt remove -y docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc
+    apt remove -y docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc
 }
 
-# Install Docker and containerd function
+# Install containerd function
 function install_containerd {
     echo "Installing Docker and containerd..."
-    sudo apt install -y ca-certificates curl
-    sudo install -m 0755 -d /etc/apt/keyrings
-    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-    sudo chmod a+r /etc/apt/keyrings/docker.asc
+    apt install -y ca-certificates curl
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    chmod a+r /etc/apt/keyrings/docker.asc
 
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-    sudo apt update
-    sudo apt install -y containerd.io
-    sudo systemctl stop containerd
-    sudo mv /etc/containerd/config.toml /etc/containerd/config.toml.orig
-    sudo containerd config default | sudo tee /etc/containerd/config.toml
-    sudo sed -i '/SystemdCgroup/s/false/true/' /etc/containerd/config.toml
-    sudo systemctl enable --now containerd
-    sudo systemctl start containerd
+    apt update
+    apt install -y containerd.io
+    apt-mark hold containerd.io
+    systemctl stop containerd
+    mv /etc/containerd/config.toml /etc/containerd/config.toml.orig
+    containerd config default | tee /etc/containerd/config.toml
+    sed -i '/SystemdCgroup/s/false/true/' /etc/containerd/config.toml
+    systemctl enable --now containerd
+    systemctl start containerd
+}
+
+# Update Kubernetes apt keyring
+function update_kubernetes_keyring {
+    echo "Updating Kubernetes apt keyring..."
+    rm -rf /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+    curl -fsSL https://pkgs.k8s.io/core:/stable:/v"${KUB_VERSION%.*}"/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+    echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v${KUB_VERSION%.*}/deb/ /" | tee /etc/apt/sources.list.d/kubernetes.list
+    apt update || { echo "Failed to update apt Kubernetes apt keyring"; exit 1; }
 }
 
 # Install Kubernetes function
 function install_kubernetes {
     echo "Installing Kubernetes..."
-    # Remove existing keyring file if it exists
-    if [ -f /etc/apt/keyrings/kubernetes-apt-keyring.gpg ]; then
-        echo "Removing existing Kubernetes APT keyring..."
-        sudo rm /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-    fi
-    echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-    curl -fsSL https://pkgs.k8s.io/core:/stable:/$KUB_VERSION/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg 
-    sudo apt update
-
-    sudo apt install -y kubeadm=$KUBELET_VERSION kubelet=$KUBELET_VERSION kubectl=$KUBELET_VERSION
-    sudo apt-mark hold containerd.io kubelet kubeadm kubectl
-    sudo kubeadm config images pull
-    sudo kubeadm init --pod-network-cidr=$POD_NETWORK_CIDR
+    apt install -y kubeadm="$KUB_VERSION"-1.1 kubelet="$KUB_VERSION"-1.1 kubectl="$KUB_VERSION"-1.1 || { echo "Failed to install Kubernetes components for version $KUB_VERSION"; exit 1; }
+    apt-mark hold kubelet kubeadm kubectl
+    kubeadm config images pull --kubernetes-version=v$KUB_VERSION
+    kubeadm init --kubernetes-version=v$KUB_VERSION --pod-network-cidr=$POD_NETWORK_CIDR
 
     mkdir -p $HOME/.kube
-    sudo cp -f /etc/kubernetes/admin.conf $HOME/.kube/config
-    sudo chown $(id -u):$(id -g) $HOME/.kube/config 
+    cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+    chown -R $(id -u $SUDO_USER):$(id -g $SUDO_USER) $HOME/.kube
 }
 
 # Configure nodes function
@@ -191,12 +196,12 @@ function install_flannel {
     kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
 }
 
-# Install CNI plugins function
+# DEPRECATED
 function install_cni_plugins {
     echo "Installing CNI plugins..."
-    sudo mkdir -p /opt/cni/bin
+    mkdir -p /opt/cni/bin
     curl -O -L https://github.com/containernetworking/plugins/releases/download/$CNI_PLUGINS_VERSION/cni-plugins-linux-amd64-$CNI_PLUGINS_VERSION.tgz
-    sudo tar -C /opt/cni/bin -xzf cni-plugins-linux-amd64-$CNI_PLUGINS_VERSION.tgz
+    tar -C /opt/cni/bin -xzf cni-plugins-linux-amd64-$CNI_PLUGINS_VERSION.tgz
     rm cni-plugins-linux-amd64-$CNI_PLUGINS_VERSION.tgz
 }
 
@@ -209,11 +214,12 @@ function configure_kube_proxy {
 function install_metallb {
     echo "Installing MetalLB..."
     kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/$METALLB_VERSION/config/manifests/metallb-native.yaml
-    sleep 10
-    ./check_metallb.sh
-    echo "$STATIC_IPS"
-    sleep 10
-    cat <<EOF > metallb-values.yml
+    ./k8s_check-metallb.sh
+    ST_IP=""
+    for IP in $(echo ${!STATIC_IPS[@]}); do
+        ST_IP+="      - ${STATIC_IPS[$IP]}"$'\n';
+    done
+    cat << EOF | kubectl apply -f -
 apiVersion: metallb.io/v1beta1
 kind: IPAddressPool
 metadata:
@@ -221,40 +227,43 @@ metadata:
     namespace: metallb-system
 spec:
     addresses:
+$ST_IP
 EOF
-    for IP in $STATIC_IPS; do
-        echo "      - ${IP}" >> metallb-values.yml
-    done
-    sleep 20
-    kubectl apply -f metallb-values.yml
 }
 
 # Apply static IPs function
 function apply_static_ips {
     echo "Applying static IPs..."
-    sudo bash -c "cat<<EOF > /etc/netplan/00-installer-config.yaml
-    network:
-        ethernets:
-            $INTERFACE:
-                dhcp4: no
-                addresses:
-                    - $NET_IP
-                routes: 
-                    - to: default
-                      via: $GATEWAY_IP
-                nameservers:
-                    addresses:
-                        - $GATEWAY_IP
-EOF"
-    sudo netplan apply
+    ST_IP=""
+    for IP in $(echo ${!STATIC_IPS[@]}); do
+        ST_IP+="        - ${STATIC_IPS[$IP]}"$'\n';
+    done
+    ST_IP=${ST_IP%$'\n'}
+    cat <<EOF > /etc/netplan/50-cloud-init.yaml
+network:
+  version: 2
+  ethernets:
+    $INTERFACE:
+      dhcp4: no
+      addresses:
+        - $NET_IP
+$ST_IP
+      routes:
+        - to: default
+          via: $GATEWAY_IP
+      nameservers:
+        addresses:
+          - $GATEWAY_IP
+EOF
+    netplan apply
 }
 
 # Create storage directories function
 function create_storage_directories {
     echo "Creating storage directories..."
-    sudo mkdir -p /storage/ssd
-    sudo mkdir -p /storage/standard
-    cat <<EOF > storageclass.yml
+    mkdir -p /storage/ssd
+    mkdir -p /storage/standard
+    cat << EOF | kubectl apply -f -
 apiVersion: storage.k8s.io/v1
 kind: StorageClass
 metadata:
@@ -279,7 +288,7 @@ reclaimPolicy: Retain
 volumeBindingMode: Immediate
 allowVolumeExpansion: true
 EOF
-    cat <<EOF > persistemtvolume.yml
+    cat << EOF | kubectl apply -f -
 apiVersion: v1
 kind: PersistentVolume
 metadata:
@@ -334,25 +343,23 @@ spec:
           values:
           - $HOST
 EOF
-    kubectl apply -f storageclass.yml
-    kubectl apply -f persistentvolume.yml
 }
 
 # Install Helm function
 function install_helm {
     echo "Installing Helm..."
-    curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
-    sudo apt install -y apt-transport-https
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
-    sudo apt update
-    sudo apt install -y helm
+    curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | tee /usr/share/keyrings/helm.gpg > /dev/null
+    apt install -y apt-transport-https
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | tee /etc/apt/sources.list.d/helm-stable-debian.list
+    apt update
+    apt install -y helm
 }
 
 # Install NGINX Ingress Controller function
 function install_nginx_ingress {
     echo "Installing NGINX Ingress Controller..."
-    helm repo add bitnami https://charts.bitnami.com/bitnami
-    helm install nginx-ingress-controller bitnami/nginx-ingress-controller --version $NGINX_HELM_VERSION
+    su - $SUDO_USER -c "helm repo add bitnami https://charts.bitnami.com/bitnami"
+    su - $SUDO_USER -c "helm install nginx-ingress-controller bitnami/nginx-ingress-controller --version $NGINX_HELM_VERSION --set controller.service.clusterIP=$(echo "$STATIC_IPS" | awk -F/ '{print $1}')"
 }
 
 # Main script execution
@@ -361,10 +368,11 @@ disable_swap
 enable_ip_forwarding
 remove_docker
 install_containerd
+update_kubernetes_keyring
 install_kubernetes
 configure_nodes
 install_flannel
-install_cni_plugins
+#install_cni_plugins
 configure_kube_proxy
 install_metallb
 apply_static_ips
